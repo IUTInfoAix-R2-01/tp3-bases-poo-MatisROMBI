7632684884100154a50634e9841be3fd918dbb5c
rombiMatis <matis.rombi@etu.univ-amu.fr>
2025-02-04 14:12:01 +0100

    Travail de début du TP3

---
+++ src/Livre.java
@@ -0,0 +1,28 @@
+
+public class Livre {
+	private String name; 
+	private double price;
+	private int quantite;
+	
+	public Livre(String name, double price, int quantite) {
+		this.name = name;
+		this.price = price;
+		this.quantite = quantite; 
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public double getPrice() {
+		return price;
+	}
+	
+	public int getQuantite() {
+		return quantite;
+	}
+	
+	public String toString() {
+		return "Livre[name=" + name +", price=" + price + ", quantite=" + quantite + "]"; 
+	}
+ }
---
+++ src/TestLivre.java
@@ -0,0 +1,7 @@
+
+public class TestLivre {
+	public static void main(String[] args) {
+		Livre l1 = new Livre("Le petit Prince", 17.50, 12);
+		System.out.println(l1.toString());
+	}
+}
